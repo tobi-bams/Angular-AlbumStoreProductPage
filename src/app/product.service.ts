fc971b542e2462f69094d40fe7517c821a2224c5
tobi-bams <teebams49@gmail.com>
2020-09-26 18:04:40 +0100

    fixing Interface

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -15,7 +15,7 @@ export class ProductService {
    }
 
    getAlbum(id: number):Observable<Album>{
-    return this._http.get(this._albumUrl).map((response) => response.json());
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
    }
 
 }
